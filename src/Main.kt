e597c090fb628509f69328d79a03261ab4d567bd
KauaTavares01 <kauatavares17@gmail.com>
2026-02-17 12:15:01 -0300

    primeiro comit

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main (){
+    val stringA = "KAUÃ LINDO"
+    val stringB = "Kauã perfeito"
+
+if (  stringA == stringB){
+    println("As strings são Iguais !!")
+
+}
+else {
+    println("As strings são Diferentes !!")
+}
+
+}
